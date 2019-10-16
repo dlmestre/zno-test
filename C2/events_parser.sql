2b0a1fab9a0a94dfc01b1ddb76f38140cd4252f8
David Mestre <davidmestre@Davids-MacBook-Pro.local>
2019-10-16 20:40:08 +0200

    Simplify sql command

--- C2/events_parser.sql
+++ C2/events_parser.sql
@@ -1,17 +1,14 @@
-WITH ntable
-     AS (SELECT customfield_publicationid    AS CustomField_PublicationId,
-                customfield_pagenum          AS CustomField_PageNum,
-                Min(timestamp)               AS dt,
+WITH rows_table
+     AS (SELECT customfield_publicationid,
+                customfield_pagenum,
+                timestamp,
                 Row_number()
                   OVER (
-                    ORDER BY Min(timestamp)) AS RowNum
-         FROM   events
-         GROUP  BY timestamp,
-                   customfield_publicationid,
-                   customfield_pagenum)
-SELECT t1.customfield_publicationid        AS CustomField_PublicationId,
-       t1.customfield_pagenum              AS CustomField_PageNum,
-       Timestampdiff(second, t1.dt, t2.dt) AS Elapsed
-FROM   ntable t1
-       INNER JOIN ntable t2
-               ON t2.rownum = t1.rownum + 1
+                    ORDER BY timestamp) AS row_id
+         FROM   events)
+SELECT t1.customfield_publicationid,
+       t1.customfield_pagenum,
+       Timestampdiff(second, t1.timestamp, t2.timestamp) AS ReadTime_sec
+FROM   rows_table t1
+       INNER JOIN rows_table t2
+               ON t2.row_id = t1.row_id + 1
